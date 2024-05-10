60fd8882f244d871e21c9d466873fb523e80d02d
Shashvat <shashvat.varma@yahoo.ca>
2024-05-10 09:06:45 -0400

    Updates to Lecture 6

--- AssignmentSubmissions/assignment_3/Assignment3Computation.py
+++ AssignmentSubmissions/assignment_3/Assignment3Computation.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import calculator as calc
+import scipy.integrate as spi
 
 #plot the grid
-
 #do the computation
 iteration_break, broken_x, broken_y, bound_x, bound_y = calc.computation(100)
 pointsize = 5
@@ -15,3 +15,14 @@ cbar.set_label("Iterations to Diverge")
 plt.xlabel('Real')
 plt.ylabel('Imaginary')
 plt.show()
+
+#define the Lorenz attractor ODE's
+def xdot(x,y,z,sigma):
+    return -sigma*(x-y)
+def ydot(x,y,z,r):
+    return -x*z + r*x - y
+def zdot(x,y,z,b):
+    return x*y - b*z
+
+#solve the system of ODE's given some initial conidtios on the coefficients and the variables. 
+sp.solve_ivp(fun = lambda t, y: [xdot(y[0],y[1],y[2],10), ydot(y[0],y[1],y[2],28), zdot(y[0],y[1],y[2],8/3)], t_span = (0,100), y0 = [1,1,1], method='RK45', t_eval = np.linspace(0,100,1000))
